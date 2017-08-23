52ac37e173ea07c21fe546092e340f20375c3a18
pmszmaglinski <pm.szmaglinski@gmail.com>
2017-08-23 10:54:16 +0200

    Update lookForIpChange.sh

--- lookForIpChange.sh
+++ lookForIpChange.sh
@@ -9,17 +9,21 @@ DOMAIN_NAME=''
 # DNS server address we look for change
 DNS_SERVER=''
 
+CHECK_INTERVAL='300'
 PID=$$
 NOTIFY_MAIL=''
 
-while [[ $(host ${DOMAIN_NAME} ${DNS_SERVER}|grep address |cut -f 4 -d ' ') == ${IP_ADDRESS} ]]; do
-        echo "Nic sie nie zmienilo"
-        sleep 5
-done
-
-if [[ $(host ${DOMAIN_NAME} ${DNS_SERVER}|grep address |cut -f 4 -d ' ') != ${IP_ADDRESS} ]]; then
+# If no serwer error = var is set
+if [[  -n $(host ${DOMAIN_NAME} ${DNS_SERVER}|grep address |cut -f 4 -d ' ') ]] && [[ $(host ${DOMAIN_NAME} ${DNS_SERVER}|grep address |cut -f 4 -d ' ') != ${IP_ADDRESS} ]]; then
 
         RECORD=$(host ${DOMAIN_NAME} ${DNS_SERVER})
         echo "Zmiana IP" | mail -s 'Zmieniony rekord na SECONDARY DNS' <<< "${RECORD}" ${NOTIFY_MAIL}
+
+else
+        # Problem with getting ip address variable (connection to dns server?, no record returned?)
+        RECORD=$(host ${DOMAIN_NAME} ${DNS_SERVER})
+        mail -s 'Error with DNS server. Exiting.' <<< "${RECORD}" ${NOTIFY_MAIL}
+
 fi
+
 kill $$
